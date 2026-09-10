db8460ff8d40aeeb386fd0c47876f03378c4b4a2
Veljko_Stojanovic <vstojanovic2023@fau.edu>
2026-09-09 23:04:00 -0400

    Fix Homework 1 code

--- Homework1/p2_Stojanovic_Veljko.py
+++ Homework1/p2_Stojanovic_Veljko.py
@@ -3,9 +3,9 @@ import math
 def find_Pythagorean(n): 
     triples = []
 
-    for a in range(1, n):
-        for b in range(a, n):
-            for c in range(1, n):
+    for a in range(1, n + 1):
+        for b in range(a, n + 1):
+            for c in range(1, n + 1):
                 if a**2 + b**2 == c**2:
                     triples.append((a, b, c))
 
